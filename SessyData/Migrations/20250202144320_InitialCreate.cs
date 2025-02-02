80526a8f11f12ec865230ff0a770195fb97e50d7
Paul Sinnema <paul@sinnema.ch>
2025-02-02 15:44:28 +0100

    Added Sessy Data layer for SQLite. Downgraded to .Net 8.0

---
+++ SessyData/Migrations/20250202144320_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SessyData.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SolarHistory",
+                columns: table => new
+                {
+                    Time = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    GlobalRadiation = table.Column<double>(type: "REAL", nullable: false),
+                    GeneratedPower = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SolarHistory", x => x.Time);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SolarHistory");
+        }
+    }
+}
