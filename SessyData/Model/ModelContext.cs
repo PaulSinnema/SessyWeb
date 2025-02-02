406d33df0a90d4c5591dc7f875ce0807abd1e9d8
Paul Sinnema <paul@sinnema.ch>
2025-02-02 15:12:15 +0100

    Couldn't load SessyWeb project. Had to enable long filenames on windows. Names got longer than 260 characters.

---
+++ SessyData/Model/ModelContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SessyData.Model
+{
+    public class ModelContext : DbContext
+    {
+        public DbSet<SolarHistory> SolarHistory { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=Sessy.db");
+    }
+
+    public class SolarHistory
+    {
+        [Key]
+        public DateTime Time { get; set; }
+        public double GlobalRadiation { get; set; }
+        public double GeneratedPower { get; set; }
+    }
+}
+
