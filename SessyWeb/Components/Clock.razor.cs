18f4b71cc94a99db6efe352ad9235b48d0cc42d0
Paul Sinnema <paul@sinnema.ch>
2025-02-18 17:21:31 +0100

    Added a clock component. First attempt on responsiveness.

---
+++ SessyWeb/Components/Clock.razor.cs
@@ -0,0 +1,38 @@
+﻿namespace SessyWeb.Components
+{
+    using Microsoft.AspNetCore.Components;
+    using SessyController.Services;
+    using System.Timers;
+
+    public partial class Clock : ComponentBase, IDisposable
+    {
+        [Inject]
+        private TimeZoneService? _timeZoneService { get; set; }
+
+        private string? CurrentTime { get; set; } = "";
+        private System.Timers.Timer? Timer;
+
+        protected override void OnInitialized()
+        {
+            Timer = new System.Timers.Timer(1000); // Update every second
+            Timer.Elapsed += (sender, args) =>
+            {
+                if (_timeZoneService != null)
+                {
+                    InvokeAsync(() =>
+                    {
+                        CurrentTime = _timeZoneService!.Now.ToString("dd MMMM yyyy  HH:mm:ss");
+                        StateHasChanged();
+                    });
+                }
+            };
+            Timer.Start();
+        }
+
+        public void Dispose()
+        {
+            Timer?.Stop();
+            Timer?.Dispose();
+        }
+    }
+}
