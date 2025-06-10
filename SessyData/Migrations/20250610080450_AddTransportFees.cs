6e81bbaf9926ad181912178bd2ac47cc252223c8
Paul Sinnema <paul@sinnema.ch>
2025-06-10 10:18:35 +0200

    Added net management costs to taxes (name is now not covering the data in the set).

---
+++ SessyData/Migrations/20250610080450_AddTransportFees.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SessyData.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTransportFees : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "CapacityTransportFee",
+                table: "Taxes",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "FixedTransportFee",
+                table: "Taxes",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CapacityTransportFee",
+                table: "Taxes");
+
+            migrationBuilder.DropColumn(
+                name: "FixedTransportFee",
+                table: "Taxes");
+        }
+    }
+}
--- SessyData/Model/Taxes.cs
+++ SessyData/Model/Taxes.cs
@@ -8,16 +8,43 @@ namespace SessyData.Model
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Auto-increment
         public int Id { get; set; }
+        /// <summary>
+        /// From when are these tariffs valid
+        /// </summary>
         public DateTime? Time { get; set; }
+        /// <summary>
+        /// Energy taxes are inclusive ODE.
+        /// </summary>
         public double EnergyTax { get; set; }
         public double ValueAddedTax { get; set; }
+        /// <summary>
+        /// What I pay the supplier for purchasing per kWh.
+        /// </summary>
         public double PurchaseCompensation { get; set; }
+        /// <summary>
+        /// What I pay the supplier for delivery per kWh.
+        /// </summary>
         public double ReturnDeliveryCompensation { get; set; }
+        /// <summary>
+        /// Yearly tax reduction from the government.
+        /// </summary>
         public double TaxReduction { get; set; }
+        /// <summary>
+        /// Yearly cost for net management (Capaciteitstarief).
+        /// </summary>
+        public double NetManagementCost { get; set; }
+        /// <summary>
+        /// Fixed fee for your connection (Vastrecht transport).
+        /// </summary>
+        public double FixedTransportFee { get; set; }
+        /// <summary>
+        /// Fee for transport depending on the capacity of the connection (Capaciteitstarief transport).
+        /// </summary>
+        public double CapacityTransportFee { get; set; }
 
         public override string ToString()
         {
-            return $"Time: {Time}, Energy tax {EnergyTax}, Value added tax {ValueAddedTax}";
+            return $"Time: {Time}, Energy tax: {EnergyTax}, Value added tax: {ValueAddedTax}, PurchaseCompensation: {PurchaseCompensation}, ReturnDeliveryCompensation: {ReturnDeliveryCompensation}, TaxReduction: {TaxReduction}, NetManagementCost: {NetManagementCost}, FixedTransportFee: {FixedTransportFee}, CapacityTransportFee: {CapacityTransportFee}";
         }
 
         public void Update(Taxes updateInfo)
@@ -28,6 +55,9 @@ namespace SessyData.Model
             PurchaseCompensation = updateInfo.PurchaseCompensation;
             ReturnDeliveryCompensation = updateInfo.ReturnDeliveryCompensation;
             TaxReduction = updateInfo.TaxReduction;
+            NetManagementCost = updateInfo.NetManagementCost;
+            FixedTransportFee = updateInfo.FixedTransportFee;
+            CapacityTransportFee = updateInfo.CapacityTransportFee;
         }
     }
 }
