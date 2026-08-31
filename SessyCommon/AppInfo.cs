1614d61810ddb3c0121a485f6f0572f6e0b67419
Paul Sinnema <paul@sinnema.ch>
2026-08-31 16:23:04 +0200

    New version

--- SessyCommon/AppInfo.cs
+++ SessyCommon/AppInfo.cs
@@ -9,6 +9,6 @@ namespace SessyCommon
     /// </summary>
     public static class AppInfo
     {
-        public const string Version = "v1.0.109";
+        public const string Version = "v1.0.110";
     }
 }
